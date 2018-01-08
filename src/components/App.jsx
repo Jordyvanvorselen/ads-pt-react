73ed37f7cf31210cf9886f5558bb8ab1f5721a47
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2018-01-08 15:53:10 +0100

    Add Detail page to application routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Route, Link } from "react-router-dom"
 import { Switch } from "react-router"
 import Overview from "../containers/Overview"
+import Detail from "../containers/Detail"
 
 const App = () => (
   <div>
@@ -12,6 +13,7 @@ const App = () => (
 
     <Switch>
       <Route exact path="/" component={Overview} />
+      <Route path="/detail" component={Detail} />
     </Switch>
   </div>
 )
