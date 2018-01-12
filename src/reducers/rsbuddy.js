0c56bbec5d068a6b6727b7dd9fec4783edb75f60
DevNebulae <gamerkeeperson+github@gmail.com>
2018-01-12 11:19:30 +0100

    Fix issue in reducers not updating loading state

--- src/reducers/rsbuddy.js
+++ src/reducers/rsbuddy.js
@@ -1,15 +1,25 @@
 import {
   RSBUDDY_FETCH_FAILED,
-  RSBUDDY_FETCH_SUCCESS
+  RSBUDDY_FETCH_SUCCESS,
+  RSBUDDY_FETCH
 } from "../actions/action-types"
 
 export function rsbuddyReducer(
   state = { data: { name: null, rsbuddy: [] }, loading: false, error: null },
-  { type, loading, error, data }
+  { type, data, error, loading }
 ) {
   switch (type) {
+    case RSBUDDY_FETCH:
+      return {
+        ...state,
+        loading
+      }
     case RSBUDDY_FETCH_FAILED:
-      return { ...state, loading, error }
+      return {
+        ...state,
+        loading,
+        error
+      }
     case RSBUDDY_FETCH_SUCCESS:
       return { data, loading, error }
     default:
